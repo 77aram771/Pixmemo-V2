ecc394bf87d7ceba6d9c541f0cace24382e6d3f6
Migue <caldeiramigue86@gmail.com>
2019-05-21 12:08:34 +0400

    Add Cropper Technologi

--- app/src/Conteiner/Cropper/index.js
+++ app/src/Conteiner/Cropper/index.js
@@ -2,39 +2,47 @@
  * Created by Aram on 17.05.2019.
  */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import ImagePicker from 'react-native-image-crop-picker';
+import {Text, View, StyleSheet, Platform, ImageStore } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import AmazingCropper from 'react-native-amazing-cropper';
 
 class Cropper extends Component {
-    constructor() {
-        super();
-        this.state = {}
+    onDone = (croppedImageUri) => {
+        console.log('croppedImageUri = ', croppedImageUri);
+        if (Platform.OS === 'ios') {
+            ImageStore.getBase64ForTag(
+                croppedImageUri,
+                (base64Image) => {
+                    // send image to server or save it locally
+                    ImageStore.removeImageForTag(croppedImageUri);
+                },
+                (err) => {}
+            );
+        }
+        else {
+            // send image to server
+        }
+        // navigate to the next page of your application
+        Actions.home();
     }
- 
-    componentWillMount = () => {
+
+    onCancel = () => {
+        // navigate back
+        Actions.pop();
     }
 
     render() {
         return (
-            <View style={styles.box}>
-                <Text style={styles.text}>
-                    Cropper
-                </Text>
-            </View>
-        )
+            <AmazingCropper
+                onDone={this.onDone}
+                onCancel={this.onCancel}
+                imageUri='https://www.lifeofpix.com/wp-content/uploads/2018/09/manhattan_-11-1600x2396.jpg'
+                imageWidth={1600}
+                imageHeight={2396}
+                NOT_SELECTED_AREA_OPACITY={0.3}
+                BORDER_WIDTH={20}
+            />
+        );
     }
 }
-
-const styles = StyleSheet.create({
-    box: {
-        backgroundColor: 'red'
-    },
-    text: {
-        fontSize: 30,
-        color: '#ffffff',
-        fontFamily: 'SEGOEPR',
-        marginBottom: 70,
-    }
-});
-
 export default Cropper;
